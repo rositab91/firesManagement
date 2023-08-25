a87e9b4b5a286c6b67cf346bc76ce25e573f9a80
rositab91 <rositab91@live.it>
2023-08-25 10:47:30 +0200

    initial commit

---
+++ src/main/java/rositabongiovanni/firesManagement/FiresManagementApplication.java
@@ -0,0 +1,13 @@
+package rositabongiovanni.firesManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiresManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiresManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rositabongiovanni/firesManagement/FiresManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package rositabongiovanni.firesManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiresManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
